a1e22fdbc8a278fff83da768cc80ff75d5d7c7c6
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-14 16:04:16 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
- import mor from "./assets/mor.png";
+import mor from "./assets/mor.png";
 
 export default function App() {
   const [local, setLocal] = useState<{
@@ -20,7 +20,7 @@ export default function App() {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: "AIzaSyDNLaY6OJ3rHnHY2pl0-3Wci3B-UzVCMKY",
   });
-    useEffect(() => {
+  useEffect(() => {
     const watcher = navigator.geolocation.watchPosition(position => {
       const {latitude, longitude} = position.coords;
       setLocal({lat: latitude, long: longitude});
@@ -30,7 +30,7 @@ export default function App() {
 
   if (!isLoaded) return <div>Loading...</div>;
 
- // const icon = "https://img.icons8.com/fluency/48/gps-device.png";
+  // const icon = "https://img.icons8.com/fluency/48/gps-device.png";
   return (
     <>
       <h1>lat :{local?.lat}</h1>
@@ -47,14 +47,23 @@ export default function App() {
           }}
         >
           {local.lat && local.long && (
-            <Marker
-              options={{label: "ola"}}
-              icon={{
-                url: mor,
-                scaledSize: new window.google.maps.Size(50, 50),
-              }}
-              position={{lat: local.lat, lng: local.long}}
-            ></Marker>
+            <>
+              <Marker
+                icon={{
+                  url: mor,
+                  scaledSize: new window.google.maps.Size(70, 70),
+                }}
+                position={{lat: local.lat, lng: local.long}}
+              ></Marker>
+
+              <Marker
+                icon={{
+                  url: mor,
+                  scaledSize: new window.google.maps.Size(70, 70),
+                }}
+                position={{lat: -23.619379, lng: -46.424244}}
+              ></Marker>
+            </>
           )}
         </GoogleMap>
       )}
