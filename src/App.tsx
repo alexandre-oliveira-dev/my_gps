ceea9239efdfae37d3b76c0aeb1e5948540dd9b0
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-14 15:11:28 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
+// import mor from "./assets/mor.png";
 
 export default function App() {
   const [local, setLocal] = useState<{
@@ -17,15 +18,16 @@ export default function App() {
     googleMapsApiKey: "AIzaSyDNLaY6OJ3rHnHY2pl0-3Wci3B-UzVCMKY",
   });
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      setLocal({
-        lat: position?.coords?.latitude,
-        long: position?.coords?.longitude,
-      });
+    const watcher = navigator.geolocation.watchPosition(position => {
+      const {latitude, longitude} = position.coords;
+      setLocal({lat: latitude, long: longitude});
     });
-    //  console.log(local);
+    return () => navigator.geolocation.clearWatch(watcher);
   }, [local]);
+
   if (!isLoaded) return <div>Loading...</div>;
+
+  const icon = "https://img.icons8.com/fluency/48/gps-device.png";
   return (
     <>
       <h1>lat :{local?.lat}</h1>
@@ -34,11 +36,16 @@ export default function App() {
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={{lat: local.lat, lng: local.long}}
-          zoom={15}
+          zoom={18}
+          clickableIcons={false}
         >
-          {local && (
+          {isLoaded && local && (
             <Marker
-              icon={{url: "https://img.icons8.com/fluency/48/gps-device.png"}}
+              icon={{
+                url: icon,
+                scaledSize: new window.google.maps.Size(50, 50),
+                anchor: new window.google.maps.Point(25, 50),
+              }}
               position={{lat: local.lat, lng: local.long}}
             ></Marker>
           )}
