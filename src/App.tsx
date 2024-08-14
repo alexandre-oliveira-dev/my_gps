32a96766f65426896e045512994a1f7f365e66fd
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-14 14:44:31 -0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import "./App.css";
+import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
 
 export default function App() {
   const [local, setLocal] = useState<{
@@ -7,20 +8,42 @@ export default function App() {
     long: number;
   }>();
 
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: "AIzaSyDNLaY6OJ3rHnHY2pl0-3Wci3B-UzVCMKY",
+  });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position.coords.latitude);
       setLocal({
         lat: position?.coords?.latitude,
         long: position?.coords?.longitude,
       });
     });
-    console.log(local);
+    //  console.log(local);
   }, [local]);
+  if (!isLoaded) return <div>Loading...</div>;
   return (
     <>
       <h1>lat :{local?.lat}</h1>
       <h1>long: {local?.long}</h1>
+      {isLoaded && local && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={{lat: local.lat, lng: local.long}}
+          zoom={15}
+        >
+          {local && (
+            <Marker
+              icon={{url: "https://img.icons8.com/fluency/48/gps-device.png"}}
+              position={{lat: local.lat, lng: local.long}}
+            ></Marker>
+          )}
+        </GoogleMap>
+      )}
     </>
   );
 }
