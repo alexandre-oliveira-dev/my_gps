1a1dcf0a69a4f1e937a390caea113a254f365cb0
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-14 15:54:39 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
-// import mor from "./assets/mor.png";
+ import mor from "./assets/mor.png";
 
 export default function App() {
   const [local, setLocal] = useState<{
     lat: number;
     long: number;
-  }>();
+  }>({
+    lat: -23.619379,
+    long: -46.424244,
+  });
 
   const containerStyle = {
     width: "100%",
@@ -17,7 +20,7 @@ export default function App() {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: "AIzaSyDNLaY6OJ3rHnHY2pl0-3Wci3B-UzVCMKY",
   });
-  useEffect(() => {
+    useEffect(() => {
     const watcher = navigator.geolocation.watchPosition(position => {
       const {latitude, longitude} = position.coords;
       setLocal({lat: latitude, long: longitude});
@@ -27,7 +30,7 @@ export default function App() {
 
   if (!isLoaded) return <div>Loading...</div>;
 
-  const icon = "https://img.icons8.com/fluency/48/gps-device.png";
+ // const icon = "https://img.icons8.com/fluency/48/gps-device.png";
   return (
     <>
       <h1>lat :{local?.lat}</h1>
@@ -38,13 +41,17 @@ export default function App() {
           center={{lat: local.lat, lng: local.long}}
           zoom={18}
           clickableIcons={false}
+          options={{
+            scaleControl: true,
+            rotateControl: true,
+          }}
         >
-          {isLoaded && local && (
+          {isLoaded && (
             <Marker
+              options={{label: "ola"}}
               icon={{
-                url: icon,
-                scaledSize: new window.google.maps.Size(50, 50),
-                anchor: new window.google.maps.Point(25, 50),
+                url: mor,
+                scaledSize: new window.google.maps.Size(500, 500),
               }}
               position={{lat: local.lat, lng: local.long}}
             ></Marker>
