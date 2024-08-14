51066daa4cd32247dae3fa0b10286fc84ece59c7
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-14 17:06:13 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import "./App.css";
 import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
 import mor from "./assets/mor.png";
-// import eu from "./assets/eu.png";
+import eu from "./assets/eu.png";
 
 export default function App() {
   const [local, setLocal] = useState<{
@@ -15,51 +15,46 @@ export default function App() {
 
   const containerStyle = {
     width: "100%",
-    height: "400px",
+    height: "100vh",
   };
 
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: "AIzaSyDNLaY6OJ3rHnHY2pl0-3Wci3B-UzVCMKY",
   });
   useEffect(() => {
-    const watcher = navigator.geolocation.watchPosition(position => {
+    navigator.geolocation.getCurrentPosition(position => {
       const {latitude, longitude} = position.coords;
       setLocal({lat: latitude, long: longitude});
     });
-    return () => navigator.geolocation.clearWatch(watcher);
-  }, [local]);
+    return () => {};
+  }, []);
 
   if (!isLoaded) return <div>Loading...</div>;
 
-  /*  const handleMapLoad = (map: google.maps.Map) => {
+  const handleMapLoad = (map: google.maps.Map) => {
     const bounds = new window.google.maps.LatLngBounds();
     bounds.extend(new window.google.maps.LatLng(local.lat, local.long));
     bounds.extend(new window.google.maps.LatLng(-23.5522944, -46.6637558));
 
     map.fitBounds(bounds);
-  }; */
+  };
 
   // const icon = "https://img.icons8.com/fluency/48/gps-device.png";
   return (
     <>
-      <h1>lat :{local?.lat}</h1>
-      <h1>long: {local?.long}</h1>
+      {/* <h1>lat :{local?.lat}</h1>
+      <h1>long: {local?.long}</h1> */}
       {isLoaded && local && (
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={{lat: local.lat, lng: local.long}}
           clickableIcons={true}
-          //  onLoad={map => handleMapLoad(map)}
+          onLoad={map => handleMapLoad(map)}
           zoom={16}
           options={{
             scaleControl: true,
             rotateControl: true,
-            isFractionalZoomEnabled: true,
-            keyboardShortcuts: true,
-            scrollwheel: true,
-            draggable: true,
-            gestureHandling: "auto",
-            zoomControl: true,
+            mapTypeControl: true,
           }}
         >
           {local.lat && local.long && (
@@ -72,14 +67,14 @@ export default function App() {
                 position={{lat: local.lat, lng: local.long}}
               ></Marker>
 
-              {/*  <Marker
+              <Marker
                 icon={{
                   url: eu,
                   scaledSize: new window.google.maps.Size(70, 70),
                   anchor: new window.google.maps.Point(25, 50),
                 }}
                 position={{lat: -23.5522944, lng: -46.6637558}}
-              ></Marker> */}
+              ></Marker>
             </>
           )}
         </GoogleMap>
